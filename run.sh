aef4b3da3c5f322d3b8f1494870b5fefb5afd315
Doron Nissimi <dnissimi@gmail.com>
2019-02-01 09:58:03 +0200

    First commit

--- run.sh
+++ run.sh
@@ -2,7 +2,7 @@
 
 #source activate cuda10
 
-for filename in ../origs/*.jpg; do
+for filename in *.jpg; do
     [ -e "$filename" ] || continue
     # ... rest of the loop body
     python imgaug-yolov3.py 50 $filename
